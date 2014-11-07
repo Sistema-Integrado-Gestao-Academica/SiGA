ec2184b060b7748211b760b6df5fda109495d690
Macário Soares <macario.junior@gmail.com>
2014-11-07 17:34:38 -0200

    Ajusting config and .htaccsess files to run codeigniter

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'sisfe';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'siga';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
