d9e119ab321438a7e5963047f8d1ae366ff203e1
Macário Soares <macario.junior@gmail.com>
2014-12-16 19:03:15 -0200

    Issue #6 - Saving graduations courses up to date

--- application/models/graduation_model.php
+++ application/models/graduation_model.php
@@ -2,4 +2,20 @@
 require_once(APPPATH."/exception/CourseNameException.php");
 class Graduation_model extends CI_Model {
 	
+	public function saveGraduation($course_name){
+		
+		$this->load->model('course_model');
+		$course_id = $this->course_model->getCourseIdByCourseName($course_name);
+		
+		$savedGraduation = $this->db->insert('graduation',array('id_course'=>$course_id));
+		
+		if($savedGraduation){
+			$insertionStatus = TRUE;
+		}else{
+			$insertionStatus = FALSE;
+		}
+		
+		return $insertionStatus;
+		
+	}
 }
