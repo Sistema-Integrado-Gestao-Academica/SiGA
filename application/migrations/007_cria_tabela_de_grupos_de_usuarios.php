ff90fd608b9472b2e8dcf2fa483af9fa7eb45980
Macário Soares <macario.junior@gmail.com>
2014-12-29 19:07:12 -0200

    Issue #11 - Criando arquivo migrations para tabela de grupos de usuário

---
+++ application/migrations/007_cria_tabela_de_grupos_de_usuarios.php
@@ -0,0 +1,17 @@
+<?php
+class Migration_Cria_tabela_de_grupos_de_usuarios extends CI_migration {
+
+	public function up() {
+		$this->dbforge->add_field(array(
+				'id_user' => array('type' => 'INT'),
+				'id_group' => array('type' => 'INT')
+		));
+		$this->dbforge->add_key('id_user', true);
+		$this->dbforge->add_key('id_group', true);
+		$this->dbforge->create_table('user_group');
+	}
+
+	public function down() {
+		$this->dbforge->drop_table('user_group');
+	}
+}
