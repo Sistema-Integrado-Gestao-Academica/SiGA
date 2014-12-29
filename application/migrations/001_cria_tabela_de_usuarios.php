28791ec96b8504ee896b3134d863c87ff6b267f2
Macário Soares <macario.junior@gmail.com>
2014-12-29 19:06:16 -0200

    Issue #11 - Ajustando arquivo migrations da tabela de usuarios

--- application/migrations/001_cria_tabela_de_usuarios.php
+++ application/migrations/001_cria_tabela_de_usuarios.php
@@ -4,16 +4,17 @@ class Migration_Cria_tabela_de_usuarios extends CI_migration {
 	public function up() {
 		$this->dbforge->add_field(array(
 			'id' => array('type' => 'INT','auto_increment' => true),
-			'nome' => array('type' => 'varchar(255)'),
-			'email' => array('type' => 'varchar(255)'),
-			'login' => array('type' => 'varchar(255)'),
-			'senha' => array('type' => 'varchar(255)')
+			'name' => array('type' => 'varchar(70)'),
+			'cpf' => array('type' => 'varchar(11)'),
+			'email' => array('type' => 'varchar(50)'),
+			'login' => array('type' => 'varchar(20)'),
+			'password' => array('type' => 'varchar(255)')
 		));
 		$this->dbforge->add_key('id', true);
-		$this->dbforge->create_table('usuarios');
+		$this->dbforge->create_table('user');
 	}
 
 	public function down() {
-		$this->dbforge->drop_table('usuarios');
+		$this->dbforge->drop_table('user');
 	}
 }
