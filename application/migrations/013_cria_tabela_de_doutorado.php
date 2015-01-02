003a6ed298e95fa26bd0f6e3275423b56d3b6d17
Macário Soares <macario.junior@gmail.com>
2015-01-02 18:30:43 -0200

    Issue #11 - Correcting doctorate table creating in migrations archives

--- application/migrations/013_cria_tabela_de_doutorado.php
+++ application/migrations/013_cria_tabela_de_doutorado.php
@@ -3,15 +3,15 @@ class Migration_Cria_tabela_de_doutorado extends CI_migration {
 
 	public function up() {
 		$this->dbforge->add_field(array(
-				'id_course' => array('type' => 'INT'),
+				'id_doctorate' => array('type' => 'INT'),
 				'id_academic_program' => array('type' => 'INT')
 		));
-		$this->dbforge->add_key('id_course');
+		$this->dbforge->add_key('id_doctorate');
 		$this->dbforge->add_key('id_academic_program');
-		$this->dbforge->create_table('graduation');
+		$this->dbforge->create_table('doctorate');
 	}
 
 	public function down() {
-		$this->dbforge->drop_table('graduation');
+		$this->dbforge->drop_table('doctorate');
 	}
 }
