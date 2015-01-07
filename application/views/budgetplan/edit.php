eb197b63e0a534e651e05eb22dd0a9fbc70ebcea
Macário Soares <macario.junior@gmail.com>
2015-01-07 17:17:47 -0200

    Issue #13 - Aplying new forms template to edit budgetplan

--- application/views/budgetplan/edit.php
+++ application/views/budgetplan/edit.php
@@ -1,56 +1,69 @@
 <h2 class="principal">Plano orçamentário</h2>
 
-<?php  
-echo form_open("budgetplan/update");
-echo form_hidden("budgetplan_id", $budgetplan['id']);
-echo form_hidden("confirm");
-
-
-echo form_label("Curso", "course");
-echo "<br>";
-echo form_dropdown('courses', $courses, $budgetplan['course_id']-1);
-
-echo "<br>";
-
-echo form_label("Montante", "amount");
-echo form_input(array(
-	"name" => "amount",
-	"id" => "amount",
-	"type" => "number",
-	"class" => "form-campo",
-	"value" => $budgetplan['amount'],
-	$disable_amount => $disable_amount
-));
-
-echo form_label("Gasto", "spending");
-echo form_input(array(
-	"name" => "spending",
-	"id" => "spending",
-	"type" => "number",
-	"class" => "form-campo",
-	"value" => $budgetplan['spending'],
-	$disable_spending => $disable_spending
-));
-
-if ($budgetplan['status'] != 4) {
-	echo form_label("Status", "status");
-	echo "<br>";
-	echo form_dropdown('status', $status, $budgetplan['status']-1, 'id="status"');
-
-	echo "<br><br>";
-
-	echo form_button(array(
-		"class" => "btn btn-primary",
-		"type" => "sumbit",
-		"content" => "Salvar",
-		"onclick" => "confirmation()"
-	));
-} else {
-	echo "<br><a href='".base_url('plano%20orcamentario')."' class='btn btn-primary'>Voltar</a>";
-}
-
-echo form_close();
-?>
+<div class="form-box-logged" id="login-box">
+<div class="header">Cadastrar um novo P.O.</div>
+		<?php form_open("budgetplan/update");
+			  echo form_hidden("budgetplan_id", $budgetplan['id']);
+			  echo form_hidden("confirm"); 
+		?>
+	<div class="body bg-gray">
+		<div class="form-group">	
+			<?php
+				echo form_label("Curso", "course");
+				echo form_dropdown('courses', $courses, $budgetplan['course_id']-1);
+			?>
+		</div>
+		<div class="form-group">	
+			<?php
+				echo form_label("Montante", "amount");
+				echo form_input(array(
+					"name" => "amount",
+					"id" => "amount",
+					"type" => "number",
+					"class" => "form-campo",
+					"value" => $budgetplan['amount'],
+					$disable_amount => $disable_amount
+				));
+			?>
+		</div>
+		<div class="form-group">	
+			<?php
+				echo form_label("Gasto", "spending");
+				echo form_input(array(
+					"name" => "spending",
+					"id" => "spending",
+					"type" => "number",
+					"class" => "form-campo",
+					"value" => $budgetplan['spending'],
+					$disable_spending => $disable_spending
+				));
+			?>
+		</div>
+		<?php if ($budgetplan['status'] != 4) {?>
+			<div class="form-group">	
+				<?php
+					echo form_label("Status", "status");
+					echo "<br>";
+					echo form_dropdown('status', $status, $budgetplan['status']-1, 'id="status"');
+				?>
+			</div>
+			<div class="footer">	
+			<?php	
+			echo form_button(array(
+					"class" => "btn bg-olive btn-block",
+					"type" => "sumbit",
+					"content" => "Salvar",
+					"onclick" => "confirmation()"
+			));
+			?>
+			</div>
+		<?php }else{
+				echo "<br><a href='".base_url('plano%20orcamentario')."' class='btn btn-primary'>Voltar</a>";
+				}
+				echo form_close();
+			?>
+	</div>
+</div>
 
 <script>
 	function confirmation() {
