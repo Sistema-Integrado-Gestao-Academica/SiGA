f95c049b4cfa6febea8bf51aa61411319147d499
Macário Soares <macario.junior@gmail.com>
2015-01-07 11:23:34 -0200

    Issue #13 - Taking login form to the new AdminLTE login form template

--- application/views/header.php
+++ application/views/header.php
@@ -10,52 +10,59 @@
 	<link rel="stylesheet" href=<?=base_url("css/bootstrap.css")?>>
 	<link rel="stylesheet" href=<?=base_url("css/estilo.css")?>>
 	<link rel="stylesheet" href=<?=base_url("font-awesome-4.2/css/font-awesome.min.css")?>>
+	<link rel="stylesheet" href=<?=base_url("css/AdminLTE.css")?>>
 	
 	<script src=<?=base_url("js/jquery-2.1.1.min.js")?>></script>
+	<script src=<?=base_url("js/bootstrap.min.js")?>></script>
+	<script src=<?=base_url("js/AdminLTE/app.js")?>></script>
 	<script src=<?=base_url("js/functions.js")?>></script>
 	<script src=<?=base_url("js/jquery.inputmask.js")?>></script>
 	<script src=<?=base_url("js/jquery.inputmask.numeric.extensions.js")?>></script>
 	<script src=<?=base_url("js/jquery.inputmask.date.extensions.js")?>></script>
 </head>
 
-<body>
-<div class="navbar navbar-inverse navbar-fixed-top">
-	<div class="container">
-		<div class="navbar-header">
-			<?=anchor("/", "Home", "class='navbar-brand'")?>
-		</div>
-		<div class="collapse navbar-collapse navbar-ex1-collapse">
-		<?php if ($session) { ?>
-			<ul class="nav navbar-nav side-nav">
-               	<?php
-               		/** 
-               		 * Variable to start the for counter in the exact middle of array user_type
-               		 * It would be in the middle because its where starts the names of user_types
-               		 */
-               		$counter = sizeof($session['user_type'])/2; 
-               		for ($i= $counter; $i < sizeof($session['user_type']) ; $i++) {
-                    	echo "<li>";
-                    		echo anchor($session['user_type'][$i],ucfirst($session['user_type'][$i]),"class='fa fa-folder-open-o'");
-                    	echo "</li>";
-					}?>
-            </ul>
-            <ul class="nav navbar-nav">
-			<?php  
-				foreach($session["user_permissions"] as $route => $permission_name){
-					echo "<li>" . anchor($route, $permission_name) . " </li>";
-				} ?>
-				<li><?=anchor("conta", "Conta")?></li>
-				<li><?=anchor("logout", "Sair")?></li>
-			</ul>
-		<?php } else { ?>
-			<ul class="nav navbar-nav ">
-				<li><?=anchor("usuario/novo", "Cadastro")?></li>
-		    </ul>
-		<?php }?>
-			
-		</div>
-	</div>
-</div>
+<body class="skin-blue">
+	<header >
+		<nav class="navbar navbar-fixed-top" role="navigation">
+			<div class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
+				<div class="">
+					<?=anchor("/", "Home", "class='navbar-brand'")?>
+				</div>
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<?php if ($session) { ?>
+					<ul class="nav navbar-nav side-nav">
+		               	<?php
+		               		/** 
+		               		 * Variable to start the for counter in the exact middle of array user_type
+		               		 * It would be in the middle because its where starts the names of user_types
+		               		 */
+		               		$counter = sizeof($session['user_type'])/2; 
+		               		for ($i= $counter; $i < sizeof($session['user_type']) ; $i++) {
+		                    	echo "<li>";
+		                    		echo anchor($session['user_type'][$i],ucfirst($session['user_type'][$i]),"class='fa fa-folder-open-o'");
+		                    	echo "</li>";
+							}?>
+		            </ul>
+		            <ul class="nav navbar-nav">
+					<?php  
+						foreach($session["user_permissions"] as $route => $permission_name){
+							echo "<li>" . anchor($route, $permission_name) . " </li>";
+						} ?>
+						<li><?=anchor("conta", "Conta")?></li>
+						<li><?=anchor("logout", "Sair")?></li>
+					</ul>
+				<?php } else { ?>
+					<ul class="nav navbar-nav ">
+						<li><?=anchor("usuario/novo", "Cadastro")?></li>
+				    </ul>
+				<?php }?>
+					
+				</div>
+			</div>
+		</nav>
+	</header>
+	
+	
 <div class="container">
 
 <?php
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -5,37 +5,47 @@ if ($sessao != NULL) { ?>
 	<h1 class="bemvindo">Bem vindo!</h1>
 <?php } else { ?>
 	<h1 class="bemvindo">Bem vindo ao SiGA</h1>
-	<h2>Login</h2>
-
-	<?php 
-	echo form_open("login/autenticar");
-
-	echo form_label("Login", "login");
-	echo form_input(array(
-		"name" => "login",
-		"id" => "login",
-		"type" => "text",
-		"class" => "form-campo",
-		"maxlength" => "255",
-		"value" => set_value("login", "")
-	));
-
-	echo form_label("Senha", "senha");
-	echo form_input(array(
-		"name" => "senha",
-		"id" => "senha",
-		"type" => "password",
-		"class" => "form-campo",
-		"maxlength" => "255"
-	));
-
-	echo "<br>";
-
-	echo form_button(array(
-		"class" => "btn btn-primary",
-		"content" => "Entrar",
-		"type" => "submit"
-	));
-
-	echo form_close();
-} ?>
+	
+	<div class="form-box" id="login-box">
+        <div class="header"><h2>Login</div>
+				<?php 
+			echo form_open("login/autenticar");
+		?>
+		<div class="form-group">
+			<?php
+			 	echo form_label("Login", "login");
+				echo form_input(array(
+					"name" => "login",
+					"id" => "login",
+					"type" => "text",
+					"class" => "form-campo",
+					"maxlength" => "255",
+					"value" => set_value("login", "")
+				)); 
+			?>
+		</div>
+		<div class="form-group">
+			<?php 
+				echo form_label("Senha", "senha");
+				echo form_input(array(
+					"name" => "senha",
+					"id" => "senha",
+					"type" => "password",
+					"class" => "form-campo",
+					"maxlength" => "255"
+				));
+			?>
+		</div>
+		<br>
+		<div class="footer">
+			<?php 
+				echo form_button(array(
+					"class" => "btn btn-primary",
+					"content" => "Entrar",
+					"type" => "submit"
+				));
+			
+				echo form_close();
+			} ?>
+		</div>
+	</div>
