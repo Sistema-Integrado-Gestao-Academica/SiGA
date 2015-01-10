83170db7dccd38ea1564c42a52d064b5c1c6cc51
italopaiva <italosensei@hotmail.com>
2015-01-10 01:38:51 -0200

    Issue #17 - Fixing migration file 09 and 10

--- application/migrations/009_cria_tabela_de_grupos_de_usuario.php
+++ application/migrations/009_cria_tabela_de_grupos_de_usuario.php
@@ -2,13 +2,46 @@
 class Migration_Cria_tabela_de_grupos_de_usuario extends CI_migration {
 
 	public function up() {
+	
 		$this->dbforge->add_field(array(
 			'id_user' => array('type' => 'INT'),
 			'id_group' => array('type' => 'INT')
 		));
-		$this->dbforge->add_key('id_user');
-		$this->dbforge->add_key('id_group');
-		$this->dbforge->create_table('user_group');
+		$this->dbforge->create_table('user_group', true);
+
+		// Adding the foreign keys constraints
+		$add_foreign_key = "ALTER TABLE user_group ADD CONSTRAINT IDUSER_USERGROUP FOREIGN KEY (id_user) REFERENCES users(id)";
+		$this->db->query($add_foreign_key);
+		
+		// $add_foreign_key = "ALTER TABLE user_group ADD CONSTRAINT IDGROUP_USERGROUP FOREIGN KEY (id_group) REFERENCES group (id_group);";
+		// $this->db->query($add_foreign_key);
+
+/*		$create_table_user_group = "
+			CREATE TABLE user_group(
+				id_user INT NOT NULL AUTO_INCREMENT,
+				id_group INT NOT NULL AUTO_INCREMENT,
+
+				CONSTRAINT IDUSER_USERGROUP_FK FOREIGN KEY (id_user) REFERENCES users(id),
+				CONSTRAINT IDGROUP_USERGROUP_FK FOREIGN KEY (id_group) REFERENCES group(id_group)
+
+			);";
+		
+		$this->db->query($create_table_user_group);*/
+
+
+	// Inserting user_group values
+		$user_group_value = array('id_user' => 1, 'id_group' => 1);
+		$this->db->insert('user_group', $user_group_value);
+
+		$user_group_value = array('id_user' => 1, 'id_group' => 2);
+		$this->db->insert('user_group', $user_group_value);
+
+		$user_group_value = array('id_user' => 2, 'id_group' => 1);
+		$this->db->insert('user_group', $user_group_value);
+
+		$user_group_value = array('id_user' => 3, 'id_group' => 2);
+		$this->db->insert('user_group', $user_group_value);
+	// End
 
 	}
 
