b1edb3dddc8575e4396c94581f7970d5d30ea871
italopaiva <italosensei@hotmail.com>
2015-01-10 19:32:39 -0200

    Issue #16 - Creating profile_route column

---
+++ application/migrations/018_cria_coluna_rota_do_perfil_de_grupos.php
@@ -0,0 +1,24 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_Cria_coluna_rota_do_perfil_de_grupos extends CI_Migration {
+
+	public function up() {
+		
+		$profile_route_column = array(
+			'profile_route' => array('type' => 'varchar(20)')
+		);
+
+		$this->dbforge->add_column('group', $profile_route_column);
+
+		// Adding the student profile route
+		$this->db->where('group_name', "estudante");
+		$this->db->update('group', array('profile_route' => "student"));
+
+	}
+
+	public function down() {
+		$this->dbforge->drop_column('group', "profile_route");
+	}
+}
+
+?>
