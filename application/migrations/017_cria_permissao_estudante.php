7c9286a3898f0931a74a93cd4091e002ac03c542
Macário Soares <macario.junior@gmail.com>
2015-01-13 13:38:46 -0200

    Issue #21 - Adjusting students permissions migration file to correct id student group

--- application/migrations/017_cria_permissao_estudante.php
+++ application/migrations/017_cria_permissao_estudante.php
@@ -9,19 +9,19 @@ class Migration_Cria_permissao_estudante extends CI_Migration {
 		$this->db->insert('permission', $student_permission);
 
 		// Creating student group
-		$student_group = array('id_group' => 8, 'group_name' => "estudante");
+		$student_group = array('id_group' => 7, 'group_name' => "estudante");
 		$this->db->insert('group', $student_group);
 
 		// Adding student permission into student group
-		$student_group_permission = array('id_group' => 8, 'id_permission' => 8);
+		$student_group_permission = array('id_group' => 7, 'id_permission' => 8);
 		$this->db->insert('group_permission', $student_group_permission);
 
 		// Add the student permission to the admin
-		$student_permission_to_admin = array('id_user' => 1, 'id_group' => 8);
+		$student_permission_to_admin = array('id_user' => 1, 'id_group' => 7);
 		$this->db->insert('user_group', $student_permission_to_admin);
 
 		// Add the student permission to the user 2
-		$student_permission = array('id_user' => 2, 'id_group' => 8);
+		$student_permission = array('id_user' => 2, 'id_group' => 7);
 		$this->db->insert('user_group', $student_permission);
 	}
 
@@ -30,10 +30,10 @@ class Migration_Cria_permissao_estudante extends CI_Migration {
 		$object = array('id_permission' => 8, 'permission_name' => "Estudante", 'route' => "student");
 		$this->db->delete('permission', $object);
 
-		$object = array('id_group' => 8, 'group_name' => "estudante");
+		$object = array('id_group' => 7, 'group_name' => "estudante");
 		$this->db->delete('group', $object);
 		
-		$object = array('id_group' => 8, 'id_permission' => 8);
+		$object = array('id_group' => 7, 'id_permission' => 8);
 		$this->db->delete('group_permission', $object);
 	}
 }
