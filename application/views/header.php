e370d3801d1dbb2307900f40244fa5a22a9f451e
Macário Soares <macario.junior@gmail.com>
2015-01-14 11:12:38 -0200

    Uncomplicating dropdown groups menu, and applying new icons in it

--- application/views/header.php
+++ application/views/header.php
@@ -128,7 +128,7 @@
 	                if(@$session['user_groups'][SECRETARIA]){
 						$secretario = $session['user_groups'][SECRETARIA];
 						echo "<li class='treeview'>";
-							echo anchor($secretario, ucfirst($secretario),"class='fa fa-folder-open-o'");
+							echo anchor($secretario, ucfirst($secretario),"class='fa fa-folder-o'");
 							echo "<ul class='treeview-menu'>";
 							
 							foreach($session['user_groups'] as $userGroupId => $userGroupName){
@@ -144,14 +144,9 @@
 							            	$permissions_routes = $session['user_permissions']['route'];
 							            	foreach($permissions_names as $id_group => $permission_name){
 							            		foreach ($permissions_routes as $id_group2 => $permission_route){
-													switch($id_group2){
-														case SECRETARIA:
-															break;
-														default: 
-															for($i=0;$i<sizeof($permission_name); $i++){
-																echo "<li>" . anchor($permission_route[$i], $permission_name[$i]) . " </li>";
-															} 
-							            			 }
+													for($i=0;$i<sizeof($permission_name); $i++){
+														echo "<li>" . anchor($permission_route[$i], $permission_name[$i], "class='fa fa-caret-right'") . " </li>";
+													} 
 							            		}
 							            	}
 							            	echo "</ul>";
@@ -167,7 +162,7 @@
 							$permissions_routes = $session['user_permissions']['route'];
 							foreach($session['user_groups'] as $userGroupId => $userGroupName){
 		                    	echo "<li class='treeview'>";
-		                    		echo anchor($userGroupName, ucfirst($userGroupName),"class='fa fa-folder-o'");
+		                    		echo anchor($userGroupName, ucfirst($userGroupName),"class='fa fa-folder-open-o'");
 		                    		echo "<ul class='treeview-menu'>";
 					            	
 					            	foreach($permissions_names as $id_group => $permission_name){
@@ -175,7 +170,7 @@
 											if ($id_group == $id_group2){
 												if( $id_group == $userGroupId || $id_group2 == $userGroupId){
 													for($i=0;$i<sizeof($permission_name); $i++){
-														echo "<li>" . anchor($permission_route[$i], $permission_name[$i],"class='fa fa-folder-open-o'") . " </li>";
+														echo "<li>" . anchor($permission_route[$i], $permission_name[$i],"class='fa fa-caret-right'") . " </li>";
 													}
 												} 
 											}
