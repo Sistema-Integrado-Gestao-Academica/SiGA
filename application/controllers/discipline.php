b08c51a5eb66af094eee5cfb5aae9aecb4034a2d
Macário Soares <macario.junior@gmail.com>
2015-01-31 12:33:56 -0200

    Issue #31 - Updating disciplines flow to new funcionality of editing

--- application/controllers/discipline.php
+++ application/controllers/discipline.php
@@ -1,6 +1,6 @@
 <?php
 class Discipline extends CI_Controller {
-
+	
 	public function discipline_index(){
 		$this->load->template("discipline/index_discipline");
 	}
@@ -14,7 +14,7 @@ class Discipline extends CI_Controller {
 	
 	public function newDiscipline(){
 		
-		$disciplineDataStatus = $this->validatesNewCourseData();
+		$disciplineDataStatus = $this->validatesDisciplineFormsData();
 		
 		if($disciplineDataStatus){
 			define('WORKLOAD_PER_CREDIT', 15);
@@ -53,6 +53,50 @@ class Discipline extends CI_Controller {
 		
 	}
 	
+	public function updateDiscipline(){
+		$disciplineDataStatus = $this->validatesDisciplineFormsData();
+		
+		if($disciplineDataStatus){
+			$disciplineName = $this->input->post('discipline_name');
+			$disciplineCode = $this->input->post('discipline_code');
+			$acronym 		 = $this->input->post('name_abbreviation');
+			$credits		 = $this->input->post('credits');
+			$workload 		 = $this->input->post('workload');
+				
+			$disciplineToUpdate = array(
+					'discipline_name'   => $disciplineName,
+					'name_abbreviation' => $acronym,
+					'credits'			=> $credits,
+					'workload' 		    => $workload
+			);
+				
+			$this->load->model('discipline_model');
+			$updated = $this->discipline_model->updateDisciplineData($disciplineCode,$disciplineToUpdate);
+			$updateStatus = "success";
+			$updateMessage = "Disciplina \"{$disciplineName}\" alterada com sucesso";
+		}else{
+			$updateStatus = "danger";
+			$updateMessage = "Dados na forma incorreta.";
+		}
+		$this->session->set_flashdata($updateStatus, $updateMessage);
+		redirect('/discipline/discipline_index');
+	}
+	
+	public function formToEditDiscipline($code){
+		$this->load->helper('url');
+		$site_url = site_url();
+		
+		$this->load->model('discipline_model');
+		$discipline_searched = $this->discipline_model->getDisciplineByCode($code);
+		$data = array(
+				'discipline' => $discipline_searched,
+				'url' => $site_url
+		);
+		
+		loadTemplateSafelyByPermission("discipline",'discipline/update_discipline', $data);
+		
+	}
+	
 	public function formToRegisterNewDiscipline(){
 		loadTemplateSafelyByPermission("discipline", "discipline/register_discipline");
 	}
@@ -60,7 +104,7 @@ class Discipline extends CI_Controller {
 	/**
 	 * Validates the data submitted on the new discipline form
 	 */
-	private function validatesNewCourseData(){
+	private function validatesDisciplineFormsData(){
 		$this->load->library("form_validation");
 		$this->form_validation->set_rules("discipline_name", "Discipline Name", "required|trim|xss_clean|callback__alpha_dash_space");
 		$this->form_validation->set_rules("discipline_code", "Discipline Code", "required");
--- application/models/discipline_model.php
+++ application/models/discipline_model.php
@@ -20,6 +20,63 @@ class Discipline_model extends CI_Model {
 		return $insertionStatus;
 	}
 	
+	public function getDisciplineByCode($discipline_code){
+		$empty = empty($discipline_code);
+		
+		if(!$empty){
+			$this->db->where('discipline_code',$discipline_code);
+			$discipline = $this->db->get('discipline')->row_array();
+		}else{
+			$discipline = FALSE;
+		}
+		return $discipline;
+	}
+	
+	public function updateDisciplineData($disciplineCode,$disciplineToUpdate){
+		$disciplineExists = $this->getDisciplineByCode($disciplineCode);
+		
+		if($disciplineExists){
+			//verify is names has changed
+			$updatedName = $disciplineToUpdate['discipline_name'];
+			$lastName = $disciplineExists['discipline_name'];
+			if($updatedName != $lastName){
+				$nameAlreadyExists = $this->checkDisciplineNameExists($updatedName);
+				if(!$nameAlreadyExists){
+					$this->updateDisciplineOnDB($disciplineCode,$disciplineToUpdate);
+				}else{
+					throw new DisciplineNameException("A disciplina '".$updatedName."' já existe.");
+				}
+			}else{
+				$this->updateDisciplineOnDB($disciplineCode,$disciplineToUpdate);
+			}
+		}else{
+			throw new DisciplineException("Impossível alterar disciplina. O código informado não existe.");
+		}
+	}
+	
+	private function updateDisciplineOnDB($disciplineCode,$disciplineToUpdate){
+		$this->db->where('discipline_code',$disciplineCode);
+		$this->db->update("discipline", $disciplineToUpdate);
+	}
+	
+	private function checkDisciplineNameExists($askedName){
+		
+		$this->db->select('discipline_name');
+		$this->db->from('discipline');
+		$this->db->where('discipline_name', $askedName);
+		$searchResult = $this->db->get();
+		
+		if($searchResult->num_rows() > 0){
+			$disciplineNameAlreadyExists = TRUE;
+		}else{
+			$disciplineNameAlreadyExists = FALSE;
+		}
+		
+		return $disciplineNameAlreadyExists;
+		
+	}
+	
+	
 	public function disciplineExists($disciplineCode, $disciplineName){
 		$this->db->where('discipline_code',$disciplineCode);
 		$disciplineCodeExists = $this->db->get('discipline')->row_array();
