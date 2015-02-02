5f749731c5d2bb3884fd1866cf08b1984a9be1ca
italopaiva <italosensei@hotmail.com>
2015-02-02 09:24:22 -0200

    Issue #32 - Fixing migration file 26 to create semester table

--- application/migrations/026_cria_tabela_de_semestre.php
+++ application/migrations/026_cria_tabela_de_semestre.php
@@ -3,24 +3,14 @@
 class Migration_Cria_tabela_de_semestre extends CI_Migration {
 
 	public function up() {
-		// Current semester table
-		$this->dbforge->add_field(array(
-			'id' => array('type' => 'INT')
-		));
-
-		$this->dbforge->create_table('current_semester', true);
-
-		// Inserting data
-		$object = array('id' => 2);
-		$this->db->insert('current_semester', $object);
-
+		
 		// Semester table
 		$this->dbforge->add_field(array(
-			'id' => array('type' => 'INT', 'auto_increment' => true),
+			'id_semester' => array('type' => 'INT', 'auto_increment' => true),
 			'description' => array('type' => 'varchar(7)')
 		));
 
-		$this->dbforge->add_key('id', true);
+		$this->dbforge->add_key('id_semester', true);
 		$this->dbforge->create_table('semester', true);
 
 		// Inserting data
@@ -31,14 +21,30 @@ class Migration_Cria_tabela_de_semestre extends CI_Migration {
 			$this->db->insert('semester', $object);
 		}
 
+		// Current semester table
+		$this->dbforge->add_field(array(
+			'id_semester' => array('type' => 'INT')
+		));
+		$this->dbforge->create_table('current_semester', true);
+
+		$id_semester_fk = "ALTER TABLE current_semester ADD CONSTRAINT IDSEMESTER_CURRENTSEM_FK FOREIGN KEY (id_semester) REFERENCES semester(id_semester)";
+		$this->db->query($id_semester_fk);
+
+		// Inserting data
+		$object = array('id_semester' => 2);
+		$this->db->insert('current_semester', $object);
 	}
 
 	public function down() {
+
+		$id_semester_fk = "ALTER TABLE current_semester DROP CONSTRAINT IDSEMESTER_CURRENTSEM_FK";
+		$this->db->query($id_semester_fk);
+
 		$this->dbforge->drop_table('semester');
 		$this->dbforge->drop_table('current_semester');
 	}
 
 }
 
-/* End of file 025_cria_tabela_de_semestre.php */
+/* End of file 026_cria_tabela_de_semestre.php */
 /* Location: ./application/migrations/025_cria_tabela_de_semestre.php */ ?>
