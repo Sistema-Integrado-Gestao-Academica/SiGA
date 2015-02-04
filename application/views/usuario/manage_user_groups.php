528b25eb942ce64cd933d1a0f3ed23479d8d15d5
italopaiva <italosensei@hotmail.com>
2015-02-04 21:12:50 -0200

    Issue #38 - Adding back button to manage groups and list groups pages

--- application/views/usuario/manage_user_groups.php
+++ application/views/usuario/manage_user_groups.php
@@ -3,4 +3,7 @@
 	displayUserGroups($idUser, $userGroups);
 	echo "<br>";
 	displayAllGroupsToUser($idUser, $allGroups, $userGroups);
+	echo "<br>";
+
+	echo anchor('user_report', "Voltar", "class='btn btn-danger'");
 ?>
