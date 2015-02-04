9e2c42480827cd2f09608dbbe1c8a10b4350fd6d
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-02-04 15:23:06 -0200

    Back button on the edit employee view

--- application/views/funcionarios/funcionario_altera.php
+++ application/views/funcionarios/funcionario_altera.php
@@ -1,37 +1,37 @@
 <div class="form-box-logged" id="login-box"> 
 	<div class="header">Alterar Funcionário</div>
-		<?php 
-			echo form_open("funcionario/altera");
-			echo form_hidden("setor_id", $funcionario['id']);
-		?>
-	<div class="body bg-gray">
-		<div class="form-group">	
-		<?php
-		echo form_label("Nome do Funcionário", "nome");
-		echo form_input(array(
-			"name" => "nome",
-			"id" => "nome",
-			"type" => "text",
-			"class" => "form-campo",
-			"class" => "form-control",
-			"maxlength" => "255",
-			"value" => set_value("nome", $funcionario['nome'])
-			));
+	<?= form_open("funcionario/altera") ?>
+		<?= form_hidden("setor_id", $funcionario['id']) ?>
+		<div class="body bg-gray">
+			<div class="form-group">	
+				<?= form_label("Nome do Funcionário", "nome") ?>
+				<?= form_input(array(
+					"name" => "nome",
+					"id" => "nome",
+					"type" => "text",
+					"class" => "form-campo",
+					"class" => "form-control",
+					"maxlength" => "255",
+					"value" => set_value("nome", $funcionario['nome'])
+					)) ?>
 
-		echo form_error("nome");
-		?>
+				<?= form_error("nome") ?>
+			</div>
 		</div>
-	</div>
+
 		<div class="footer">
-			<?php 
-			echo form_button(array(
-				"class" => "btn bg-olive btn-block",
-				"content" => "Alterar",
-				"type" => "sumbit"
-			));
-			
-			echo form_close();
-			?>
+			<div class="row">
+				<div class="col-xs-6">
+					<?= form_button(array(
+						"class" => "btn bg-olive btn-block",
+						"content" => "Alterar",
+						"type" => "sumbit"
+					)) ?>
+				</div>
+				<div class="col-xs-6">
+					<?= anchor('funcionarios', 'Voltar', "class='btn bg-olive btn-block'") ?>
+				</div>
+			</div>
 		</div>
-		
+	<?= form_close() ?>
 </div>
