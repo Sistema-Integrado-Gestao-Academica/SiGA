4bc06be6342b69e2001773d9730632cd13ed5f89
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2015-02-04 15:40:38 -0200

    Back button on the sector view

--- application/views/setores/formulario_altera.php
+++ application/views/setores/formulario_altera.php
@@ -1,38 +1,36 @@
 <div class="form-box-logged" id="login-box"> 
 	<div class="header">Alterar Setor</div>
-		<?php
-			echo form_open("setor/altera");
-			echo form_hidden("setor_id", $setor['id']);
-		?>
-	<div class="body bg-gray">
-		<div class="form-group">	
-		<?php
-		echo form_label("Cadastrar um novo setor", "nome");
-		echo form_label("Nome do setor", "nome");
-		echo form_input(array(
-			"name" => "nome",
-			"id" => "nome",
-			"type" => "text",
-			"class" => "form-control",
-			"class" => "form-campo",
-			"maxlength" => "255",
-			"value" => set_value("nome", $setor['nome'])
-		));
-		echo form_error("nome");
-		
-		?>
+	<?= form_open("setor/altera") ?>
+		<?= form_hidden("setor_id", $setor['id']) ?>
+		<div class="body bg-gray">
+			<div class="form-group">	
+				<?= form_label("Nome do setor", "nome") ?>
+				<?= form_input(array(
+					"name" => "nome",
+					"id" => "nome",
+					"type" => "text",
+					"class" => "form-control",
+					"class" => "form-campo",
+					"maxlength" => "255",
+					"value" => set_value("nome", $setor['nome'])
+				)) ?>
+				<?= form_error("nome") ?>
+			</div>
 		</div>
-	</div>
+
 		<div class="footer">
-			<?php 
-			echo form_button(array(
-				"class" => "btn bg-olive btn-block",
-				"content" => "Alterar",
-				"type" => "sumbit"
-			));
-			
-			echo form_close();
-			?>
+			<div class="row">
+				<div class="col-xs-6">
+					<?= form_button(array(
+						"class" => "btn bg-olive btn-block",
+						"content" => "Alterar",
+						"type" => "sumbit"
+					)) ?>
+				</div>
+				<div class="col-xs-6">
+					<?= anchor('setores', 'Voltar', "class='btn bg-olive btn-block'") ?>
+				</div>
+			</div>
 		</div>
-		
+	<?= form_close() ?>
 </div>
