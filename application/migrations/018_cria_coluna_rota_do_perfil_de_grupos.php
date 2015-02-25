f17a25f2e377de8f52a3827c347478d0538c9a07
Macário Soares <macario.junior@gmail.com>
2015-02-25 16:24:16 -0300

    Issue #34 - Adjusting migration 18 to make null by default value for profile routes in group table

--- application/migrations/018_cria_coluna_rota_do_perfil_de_grupos.php
+++ application/migrations/018_cria_coluna_rota_do_perfil_de_grupos.php
@@ -5,11 +5,14 @@ class Migration_Cria_coluna_rota_do_perfil_de_grupos extends CI_Migration {
 	public function up() {
 		
 		$profile_route_column = array(
-			'profile_route' => array('type' => 'varchar(20)')
+			'profile_route' => array('type' => 'varchar(20)','NULL'=>TRUE)
 		);
 
 		$this->dbforge->add_column('group', $profile_route_column);
-
+		
+		$addConstraint = "ALTER TABLE `group` ADD UNIQUE(`group_name`)";
+		$this->db->query($addConstraint);
+		
 		// Adding the student profile route
 		$this->db->where('group_name', "estudante");
 		$this->db->update('group', array('profile_route' => "student"));
