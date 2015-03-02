fcd15b842ff270f2d2a1555044101dad76162a04
Macário Soares <macario.junior@gmail.com>
2015-03-02 19:54:21 -0300

    Issue #44 - Updating student home to show the discipline offers list of his course

--- application/views/usuario/student_home.php
+++ application/views/usuario/student_home.php
@@ -1,3 +1,4 @@
+<?php   $session = $this->session->userdata("current_user");?>
 <br><br><br>
 <div class="col-lg-12 col-xs-6">
 	<div class="small-box bg-green">
@@ -28,5 +29,39 @@
 		    Mais informações <i class="fa fa-arrow-circle-right"></i>
 		</a>
 	</div>
+</div>
+<?php 
+	require_once APPPATH.'controllers/offer.php';
+	$offer = new Offer();
+	$disciplines = $offer->displayOfferedDisciplines($course['id_course']);
+?>
+
+<div class="col-lg-12 callout callout-info">
+	<h3>Lista de Ofertas</h3>
+	
+	<?php if($disciplines){ ?>
+	<table class="table table-striped table-bordered">
+		<tr>
+			<td><h3 class="text-center">Nome da disciplina</h3></td>
+			<td><h3 class="text-center">Código da disciplina</h3></td>
+			<td><h3 class="text-center">Créditos</h3></td>
+		</tr>
+		<?php 
+			foreach ($disciplines as $rows){
+				foreach ($rows as $discipline){	
+					echo "<tr>";	
+					echo "<td>".$discipline['discipline_name']."</td>";
+					echo "<td>".$discipline['discipline_code']."</td>";
+					echo "<td>".$discipline['credits']."</td>";
+					echo "</tr>";
+				}
+			}
+			?>
+	</table>
+	<?php }else{?>
+	
+	<h3><label class="label label-default"> A lista de ofertas do seu curso ainda não foi produzida.</label></h3>
+	
+	<?php }?>
 </div>
 
