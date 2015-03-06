bcd2066225e7b3eb02c2acd723cddcfef28f0e66
Macário Soares <macario.junior@gmail.com>
2015-03-06 12:02:37 -0300

    Issue #49 - Updating course_index view page to show academic secretary only his own course, and granting only edid course action

--- application/views/course/course_index.php
+++ application/views/course/course_index.php
@@ -1,39 +1,75 @@
-<h2 class="principal">Menu de cursos</h2>
+<?php 
+	$session = $this->session->userdata("current_user");
+	require_once APPPATH.'controllers/course.php';
+?>
 
-<?= anchor("course/formToRegisterNewCourse", "Cadastrar Curso", array(
-	"class" => "btn btn-primary",
-	"type" => "submit",
-	"content" => "newCourse"
-)) ?>
+<?php
+$userIsAcademicSecretary = array_key_exists('courseSecretaryAcademic', $session['user_permissions']);
 
+if ($userIsAcademicSecretary):
+	$courseController = new Course();
+	
+	$coursesForSecretary = $courseController->getCoursesOfSecretary($session['user']['id']);
+?>
+<h2 class="principal">Meus cursos</h2>
 <br><br>
-
-<table class="table table-striped table-bordered">
-	<tr>
-		<td><h3 class="text-center">Cursos Cadastrados</h3></td>
-		<td><h3 class="text-center">Ações</h3></td>
-	</tr>
-	<?php if ($courses !== FALSE): ?>
-		<?php foreach($courses as $course): ?>
-			<tr>
-				<td class="text-center"><?= $course['course_name'] ?></td>
-
-				<td>
-					<?= anchor("course/formToEditCourse/{$course['id_course']}", "<span class='glyphicon glyphicon-edit'></span>", "class='btn btn-primary' style='margin-right:5%;'") ?>
-					<?= anchor("course/deleteCourse/{$course['id_course']}", "<span class='glyphicon glyphicon-remove'></span>", "class='btn btn-danger'") ?>
-				</td>
-			</tr>
-		<?php endforeach ?>
-	<?php else: ?>
+	
+	<table class="table table-striped table-bordered">
 		<tr>
-			<td><h3><label class="label label-default"> Não existem cursos cadastrados</label></h3></td>
+			<td><h3 class="text-center">Cursos Cadastrados</h3></td>
+			<td><h3 class="text-center">Ações</h3></td>
 		</tr>
-	<?php endif ?>
-</table>
-
-<br>
-<br>
-
+			<?php foreach($coursesForSecretary as $course): ?>
+				<tr>
+					<td class="text-center"><?= $course['course_name'] ?></td>
+	
+					<td>
+						<?= anchor("course/formToEditCourse/{$course['id_course']}", "<span class='glyphicon glyphicon-edit'></span>", "class='btn btn-primary' style='margin-right:5%;'") ?>
+					</td>
+				</tr>
+			<?php endforeach ?>
+	</table>
+	
+	<br>
+	<br>
+<?php 
+else :
+?>
+<h2 class="principal">Menu de cursos</h2>
+	<?= anchor("course/formToRegisterNewCourse", "Cadastrar Curso", array(
+		"class" => "btn btn-primary",
+		"type" => "submit",
+		"content" => "newCourse"
+	)) ?>
+	
+	<br><br>
+	
+	<table class="table table-striped table-bordered">
+		<tr>
+			<td><h3 class="text-center">Cursos Cadastrados</h3></td>
+			<td><h3 class="text-center">Ações</h3></td>
+		</tr>
+		<?php if ($courses !== FALSE): ?>
+			<?php foreach($courses as $course): ?>
+				<tr>
+					<td class="text-center"><?= $course['course_name'] ?></td>
+	
+					<td>
+						<?= anchor("course/formToEditCourse/{$course['id_course']}", "<span class='glyphicon glyphicon-edit'></span>", "class='btn btn-primary' style='margin-right:5%;'") ?>
+						<?= anchor("course/deleteCourse/{$course['id_course']}", "<span class='glyphicon glyphicon-remove'></span>", "class='btn btn-danger'") ?>
+					</td>
+				</tr>
+			<?php endforeach ?>
+		<?php else: ?>
+			<tr>
+				<td><h3><label class="label label-default"> Não existem cursos cadastrados</label></h3></td>
+			</tr>
+		<?php endif ?>
+	</table>
+	
+	<br>
+	<br>
+<?php endif;?>
 <?= anchor("program/registerNewProgram", "Cadastrar Programa", "class='btn btn-primary'") ?>
 
 <br><br>
