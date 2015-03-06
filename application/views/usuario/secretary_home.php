adee48d8b82aa8bead6d458d06f5532131e03a6a
Macário Soares <macario.junior@gmail.com>
2015-03-06 12:13:10 -0300

    Issue #49 - Secretaries has now specific  menus in secretary_home

--- application/views/usuario/secretary_home.php
+++ application/views/usuario/secretary_home.php
@@ -1,11 +1,15 @@
+<?php 
+	$session = $this->session->userdata("current_user");
+	$userIsAcademicSecretary = array_key_exists('courseSecretaryAcademic', $session['user_permissions']);
+?>
 <br>
 <br>
 <br>
-
+<?php  if ($userIsAcademicSecretary):?>
 <div class="col-lg-12 col-xs-6">
 	<div class="small-box bg-blue">
 		<div class="inner">
-		    <h2 align="center">Bem vindo a Secretaria!</h2>
+		    <h2 align="center">Bem vindo a Secretaria Acadêmica!</h2>
 
 		    <p>
 
@@ -22,3 +26,23 @@
 		</div>
 	</div>
 </div>
+<?php else:?>
+<div class="col-lg-12 col-xs-6">
+	<div class="small-box bg-blue">
+		<div class="inner">
+		    <h2 align="center">Bem vindo a Secretaria Financeira!</h2>
+
+		    <p>
+
+		    <div class="list-group">
+				<?=anchor('planoorcamentario', "Cadastrar Plano Orçmentário", "class='list-group-item' style='width:30%;'");?>
+			</div>
+        	</p>
+
+		</div>
+		<div class="icon">
+		    <i class="fa fa-book"></i>
+		</div>
+	</div>
+</div>
+<?php endif;?>
