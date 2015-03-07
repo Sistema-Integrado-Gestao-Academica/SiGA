4db69ddc79adfadbbf25554a8f031f3e571af8b4
italopaiva <italosensei@hotmail.com>
2015-03-06 22:25:07 -0300

    Issue #45 - Creating status column to request_discipline

---
+++ application/migrations/046_adiciona_coluna_status_em_request_discipline.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_adiciona_coluna_status_em_request_discipline extends CI_Migration {
+
+	public function up() {
+
+		$this->dbforge->add_column('request_discipline', array(
+			'status' => array('type' => 'varchar(15)')
+		));
+
+	}
+
+	public function down() {
+
+		$this->db->drop_column('request_discipline', 'status');
+	}
+}
