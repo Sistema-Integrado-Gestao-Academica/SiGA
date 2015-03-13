b5dff3f5a3622c76863c4307878c1f9e25047321
italopaiva <italosensei@hotmail.com>
2015-03-13 14:40:11 -0300

    Issue #47 - Fixing bug when getting a specific class hour to display

--- application/models/schedule_model.php
+++ application/models/schedule_model.php
@@ -125,6 +125,7 @@ class Schedule_model extends CI_Model {
 		$this->db->select('discipline_schedule.*');
 		$this->db->from('discipline_schedule');
 		$this->db->join('class_hour', "discipline_schedule.id_class_hour = class_hour.id_class_hour");
+		$this->db->where('discipline_schedule.id_offer_discipline', $idOfferDiscipline);
 		$this->db->where('class_hour.hour', $hour);
 		$this->db->where('class_hour.day', $day);
 		$foundClassHour = $this->db->get()->row_array();
