92552c852f5b9c0745d1d5b4e6bca71d59b1f443
italopaiva <italosensei@hotmail.com>
2015-03-20 12:37:14 -0300

    Issue #52 - Adding the request report to secreatary menu

--- application/views/usuario/secretary_home.php
+++ application/views/usuario/secretary_home.php
@@ -5,7 +5,7 @@
 <br>
 <br>
 <br>
-<?php  if ($userIsAcademicSecretary):?>
+<?php if ($userIsAcademicSecretary):?>
 <div class="col-lg-12 col-xs-6">
 	<div class="small-box bg-blue">
 		<div class="inner">
@@ -15,6 +15,7 @@
 
 		    <div class="list-group">
 				<?=anchor('usuario/secretary_enrollStudent', "Matricular Alunos", "class='list-group-item' style='width:20%;'");?>
+				<?=anchor('usuario/secretary_requestReport', "Relatório de Solicitações", "class='list-group-item' style='width:20%;'");?>
 				<?=anchor('usuario/secretary_offerList', "Listas de Oferta", "class='list-group-item' style='width:20%;'");?>
 				<?=anchor('usuario/secretary_courseSyllabus', "Currículos de cursos", "class='list-group-item' style='width:20%;'");?>
 			</div>
