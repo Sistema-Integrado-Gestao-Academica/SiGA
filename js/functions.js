be236999c94208d5ad6a6c05c40ef8418c286ce9
Macário Soares <macario.junior@gmail.com>
2015-03-29 20:20:51 -0300

    Issue #56 - Deleting dead code used on AJAX functions

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,5 @@
 
 $(document).ready(function(){
-	
-	evaluatesCourseType();
-
-	$("#courseType").change(function(){
-		evaluatesCourseType();
-	});
 
 	$("#search_student_btn").click(function(){
 		searchForStudent();
@@ -44,65 +38,6 @@ function searchForStudent(){
 			$("#search_student_result").html(data);
 		}
 	);
-}
-
-// Course functions
-function evaluatesCourseType(){
-	
-	var choosenCourseType = getChoosenCourseType();
-
-	var urlToPost = choosenCourseType.siteUrl + "/course/checkChoosenCourseType";
-	$.post(
-		urlToPost,
-		{courseType: choosenCourseType.courseType}, 
-		function(data){
-			$("#post_grad_types").html(data);
-
-			evaluatesPostGraduationType();
-			$("#post_graduation_type").change(function(){
-				evaluatesPostGraduationType();
-			});
-		}
-	);
-}
-
-function getChoosenCourseType(){
-
-	var siteUrl = $("#site_url").val();
-	var choosenCourseType = $("#courseType").val();
-
-	var choosenType = {
-		siteUrl: siteUrl,
-		courseType: choosenCourseType
-	};
-
-	return choosenType;
-}
-
-function evaluatesPostGraduationType(){
-
-	var choosenPostGraduationType = getChoosenPostGradType();
-	var urlToPost = choosenPostGraduationType.siteUrl + "/course/checkChoosenPostGraduationType";
-	
-	$.post(
-		urlToPost,
-		{postGradType: choosenPostGraduationType.postGradType},
-		function(data){
-			$("#chosen_post_grad_type").html(data);
-		}
-	);
-}
-
-function getChoosenPostGradType(){
-	var siteUrl = $("#site_url").val();
-	var choosenPostGraduationType = $("#post_graduation_type").val();
-
-	var choosenType = {
-		siteUrl: siteUrl,
-		postGradType: choosenPostGraduationType
-	};
-
-	return choosenType;
 }
 
 function getCurrentCourse(){
