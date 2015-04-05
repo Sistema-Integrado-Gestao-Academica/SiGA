a9f232a3f93051978bca02dd439d2879aa62cd6c
italopaiva <italosensei@hotmail.com>
2015-04-05 13:26:29 -0300

    Issue #64 - Creating constant abstract class and enrollment constants class

---
+++ application/constants/EnrollmentConstants.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('constants.php');
+
+class EnrollmentConstants extends Constants{
+	
+	const PRE_ENROLLED_STATUS = "pre_enrolled";
+	const ENROLLED_STATUS = "enrolled";
+	const NO_VACANCY_STATUS = "no_vacancy";
+
+}
