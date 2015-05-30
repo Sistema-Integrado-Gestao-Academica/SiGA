254006d77050ecc40f9ca8bc5bc60d3814cc2110
italopaiva <italosensei@hotmail.com>
2015-05-30 19:22:55 -0300

    Issue #79 - Fixing the migration file 059

--- application/migrations/059_adiciona_coluna_peso_padrao_para_dimensao.php
+++ application/migrations/059_adiciona_coluna_peso_padrao_para_dimensao.php
@@ -4,11 +4,15 @@ class Migration_adiciona_coluna_peso_padrao_para_dimensao extends CI_Migration {
 
 	public function up() {
 
-		$this->dbforge->add_column('dimension_type', array(
-			'default_weight' => array('type' => 'DOUBLE')
-		));
+		// $this->dbforge->add_column('dimension_type', array(
+		// 	'default_weight' => array('type' => 'DOUBLE')
+		// ));
 		
-		$this->db->update('dimension_type', array('default_weight' => 20));
+		// $this->db->update('dimension_type', array('default_weight' => 20));
+
+		$this->dbforge->modify_column('evaluation_dimension', array(
+			'weight' => array('type' => 'DOUBLE')
+		));
 
 	}
 
