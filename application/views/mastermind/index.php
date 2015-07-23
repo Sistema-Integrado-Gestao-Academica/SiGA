46f5c232dc8b2987d1c3b5de7c089a1836ac65e0
Macário Soares <macario.junior@gmail.com>
2015-07-23 14:30:46 -0300

    Creating index page for mastermind

--- application/views/mastermind/index.php
+++ application/views/mastermind/index.php
@@ -1,1 +1,23 @@
-<?php
+
+<br>
+<br>
+<div class="col-lg-12 col-xs-6">
+	<div class="small-box bg-blue">
+		<div class="inner">
+		    <h2 align="center">Bem vindo a página de Professor!</h2>
+		   
+			<div class = "row">
+				<div class="col-lg-6">
+				    <p>
+					    <div class="list-group" align='center'>
+							<?php echo anchor('mastermind/titling_area', "Atualize sua titulacao", "class='list-group-item' style='width:70%;'");?>
+						</div>
+		        	</p>
+			    </div>
+			</div>
+ 			<div class="icon">
+		    	<i class="fa fa-user"></i>
+			</div>
+		</div>
+	</div>
+</div>
