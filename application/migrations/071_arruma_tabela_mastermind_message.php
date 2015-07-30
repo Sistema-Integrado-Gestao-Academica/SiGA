7e364ba0edf3e53f674d2fe3f9f401ea4e4591d7
italopaiva <italosensei@hotmail.com>
2015-07-30 00:43:06 -0300

    Issue #95 - Fixing mastermind message table

---
+++ application/migrations/071_arruma_tabela_mastermind_message.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_arruma_tabela_mastermind_message extends CI_Migration {
+
+	public function up() {
+
+		// Dropping the id_student column
+		$this->dbforge->drop_column('mastermind_message', 'id_student');
+		
+		// Dropping the id_offer_discipline
+		$drop_fk = "ALTER TABLE mastermind_message DROP FOREIGN KEY mastermind_message_ibfk_1";
+		$this->db->query($drop_fk);
+		$this->dbforge->drop_column('mastermind_message', 'id_offer_discipline');
+
+		// Setting the message column to TEXT type
+		$this->dbforge->modify_column('mastermind_message', array(
+			'message' => array('type' => 'TEXT', 'null' => TRUE)
+		));
+
+		$drop_fk = "ALTER TABLE mastermind_message DROP FOREIGN KEY IDMASTERMIND_FK";
+		$this->db->query($drop_fk);
+	}
+
+	public function down() {
+
+	}
+}
