a089b937b4c9417f67c63062e05f4d14627d1582
Macário Soares <macario.junior@gmail.com>
2015-08-23 23:45:05 -0300

    Issue #97 - Creating initial page to select the report type to show

---
+++ application/views/coordinator/course_reports.php
@@ -0,0 +1,39 @@
+<br>
+<br>
+
+<div class="col-lg-12 ">
+	<div class='panel panel-primary'>
+		 
+		<div class='panel-heading'><h4>Relatórios disponíveis :</h4></div>
+			<div class='panel-body'>
+				<div class='modal-info'>
+					<div class='modal-content'>
+						<div class='modal-header bg-news'>
+							<h4 class='model-title'>Discentes</h4>
+						</div>
+						<div class='modal-body'>
+							<h3>
+								<?=anchor("coordinator/students_report", "Relatório de discentes", "class='btn btn-success'");?>		
+							</h3>
+						</div>
+						<div class='modal-header bg-news'>
+							<h4 class='model-title'>Docentes</h4>
+						</div>
+						<div class='modal-body'>
+							<h3>
+								<?=anchor("coordinator/mastermind_report", "Relatório de docentes", "class='btn btn-success'");?>								
+							</h3>
+						</div>
+						<div class='modal-header bg-news'>
+							<h4 class='model-title'>Funcionários</h4>
+						</div>
+						<div class='modal-body'>
+							<h3>
+								<?=anchor("coordinator/secretary_report", "Relatório de Funcionários", "class='btn btn-success'");?>		
+							</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+</div>
