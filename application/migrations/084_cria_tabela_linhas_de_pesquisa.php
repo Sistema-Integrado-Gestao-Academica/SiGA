c942507e9617c32fecb916af42903ed6e34646b1
Macário Soares <macario.junior@gmail.com>
2015-09-08 12:05:59 -0300

    Issue #111 - Adjusting migration file, creating index for table

--- application/migrations/084_cria_tabela_linhas_de_pesquisa.php
+++ application/migrations/084_cria_tabela_linhas_de_pesquisa.php
@@ -5,10 +5,11 @@ class Migration_cria_tabela_linhas_de_pesquisa extends CI_Migration {
 	public function up() {
 
 		$this->dbforge->add_field(array(
-			'id_research_line' => array('type' => 'INT'),
+			'id_research_line' => array('type' => 'INT', 'auto_increment' => true),
 			'id_course' => array('type' => 'INT'),
 			'description' => array('type' => 'varchar(800)')
 		));
+		$this->dbforge->add_key('id_research_line', true);
 		$this->dbforge->create_table('research_lines', TRUE);
 		
 		$addConstraint = "ALTER TABLE research_lines ADD CONSTRAINT RESEARCH_LINE_COURSE_FK FOREIGN KEY (id_course) REFERENCES course(id_course) ON DELETE CASCADE ON UPDATE RESTRICT";
