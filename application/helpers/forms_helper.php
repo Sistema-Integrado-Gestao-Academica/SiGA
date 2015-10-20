070987ac701947771ba3d3db7285a91ff8ae4357
Macário Soares <macario.junior@gmail.com>
2015-10-20 14:16:15 -0200

    Issue #127 - Adding register staff form

--- application/helpers/forms_helper.php
+++ application/helpers/forms_helper.php
@@ -807,6 +807,160 @@ function relateDisciplineToResearchLineForm($researchLines, $discipline, $syllab
 		
 }
 
+function loadStaffRegistrationForm($users){
+	
+	/**
+	 *	New staff Labels
+	 */
+	$submitBtn = array(
+			"class" => "btn bg-olive btn-block",
+			"content" => "Salvar",
+			"type" => "submit"
+	);
+	
+	$pisNumber = array(
+			"name" => "pis",
+			"id" => "pis",
+			"type" => "text",
+			"class" => "form-campo",
+			"class" => "form-control",
+			"maxlength" => "20"
+	);
+
+	$registration = array(
+			"name" => "registration",
+			"id" => "registration",
+			"type" => "text",
+			"class" => "form-campo",
+			"class" => "form-control",
+			"maxlength" => "10",
+			"placeholder" => "Opcional"
+	);
+
+	$landingDate = array(
+			"name" => "landingDate",
+			"id" => "arrivalInBrazil",
+			"type" => "text",
+			"class" => "form-campo",
+			"class" => "form-control",
+			"placeholder" => "Opcional"
+	);
+
+	$address = array(
+			"name" => "address",
+			"id" => "address",
+			"type" => "text",
+			"class" => "form-campo",
+			"class" => "form-control",
+			"maxlength" => "50"
+	);
+
+	$phone = array(
+			"name" => "telephone",
+			"id" => "telephone",
+			"type" => "text",
+			"class" => "form-campo",
+			"class" => "form-control",
+			"maxlength" => "15"
+	);
+
+	/**
+	 *	Bank labels
+	 */
+
+	$bank = array(
+			"name" => "bank",
+			"id" => "bank",
+			"type" => "text",
+			"class" => "form-campo",
+			"class" => "form-control",
+			"maxlength" => "25"
+	);
+
+	$agency = array(
+			"name" => "agency",
+			"id" => "agency",
+			"type" => "text",
+			"class" => "form-campo",
+			"class" => "form-control",
+			"maxlength" => "10"
+	);
+
+	$checkingAccount = array(
+			"name" => "accountNumber",
+			"id" => "accountNumber",
+			"type" => "text",
+			"class" => "form-campo",
+			"class" => "form-control",
+			"maxlength" => "15"
+	);
+	
+	echo "<div class='form-box' id='login-box'>";
+		echo "<div class='header'>Cadastrar novo Funcionário</div>";
+	
+		echo form_open('staff/newStaff','');
+		echo "<div class='body bg-gray'>";
+			echo "<div class='form-group'>";
+				echo form_label("PIS/INSS", "pis_number");
+				echo form_input($pisNumber);
+				echo form_error("pis_number");
+			echo "</div>";
+			echo "<div class='form-group'>";
+				echo form_label("Selecione o Funcionário", "staff");
+				echo form_dropdown("staff", $users, "id='staff'");
+				echo form_error("staff");
+			echo "</div>";
+			echo "<div class='form-group'>";
+				echo form_label("Matrícula", "registration");
+				echo form_input($registration);
+				echo form_error("registration");
+			echo "</div>";
+			echo "<div class='form-group'>";
+				echo form_label("Chegada ao Brasil", "landingDate");
+				echo form_input($landingDate);
+				echo form_error("landingDate");
+			echo "</div>";
+			echo "<div class='form-group'>";
+				echo form_label("Endereço", "address");
+				echo form_input($address);
+				echo form_error("address");
+			echo "</div>";
+			echo "<div class='form-group'>";
+				echo form_label("Telefone", "telephone");
+				echo form_input($phone);
+				echo form_error("telephone");
+			echo "</div>";
+
+			echo "<hr>";
+			echo "<h3>Dados Bancários</h3> (Opcionais)";
+
+			echo "<div class='form-group'>";
+				echo form_label("Banco", "bank");
+				echo form_input($bank);
+				echo form_error("bank");
+			echo "</div>";
+			echo "<div class='form-group'>";
+				echo form_label("Agência", "agency");
+				echo form_input($agency);
+				echo form_error("agency");
+			echo "</div>";
+			echo "<div class='form-group'>";
+				echo form_label("Conta Corrente", "accountNumber");
+				echo form_input($checkingAccount);
+				echo form_error("accountNumber");
+			echo "</div>";
+
+		echo "</div>";
+	
+		echo "<div class='footer body bg-gray'>";
+		echo form_button($submitBtn);
+		echo "</div>";
+		
+		echo form_close();
+	echo "</div>";
+
+}
+
 
 function emptyDiv(){
 	echo "";
