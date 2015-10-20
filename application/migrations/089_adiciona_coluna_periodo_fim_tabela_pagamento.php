aa8f93697612a703b91ccd0d79bb8e4bceb3e693
Italo Paiva <italosensei@hotmail.com>
2015-10-20 00:07:32 -0200

    Issue #128 - Fixing payment table attributes

---
+++ application/migrations/089_adiciona_coluna_periodo_fim_tabela_pagamento.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_adiciona_coluna_periodo_fim_tabela_pagamento extends CI_Migration {
+
+    public function up() {
+
+        $this->dbforge->add_column('payment', array(
+            'end_period' => array('type' => "varchar(10)")
+        ));
+
+        $this->dbforge->modify_column('payment', array(
+            'arrivalInBrazil' => array('type' => "varchar(10)"),
+            'installment_date_1' => array('type' => "varchar(10)"),
+            'installment_date_2' => array('type' => "varchar(10)"),
+            'installment_date_3' => array('type' => "varchar(10)"),
+            'installment_date_4' => array('type' => "varchar(10)"),
+            'installment_date_5' => array('type' => "varchar(10)")
+        ));
+
+
+    }
+
+    public function down() {
+
+    }
+
+}
