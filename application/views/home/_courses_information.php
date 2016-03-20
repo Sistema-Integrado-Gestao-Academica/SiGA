004a8320edc6152c2a25de508b533c2575ce5dc1
Emiliemorais <emilie.mo@hotmail.com>
2016-03-20 00:51:20 -0300

    ...

---
+++ application/views/home/_courses_information.php
@@ -0,0 +1,66 @@
+<?php            				
+	
+	$coursesName = $coursesProgram['coursesName'];
+
+	if(!empty($coursesName)){ ?>
+
+		<div class="panel box box-primary">
+			<div class="box-header with-border">
+	    		<h4 class="box-title">
+	      		<a data-toggle="collapse" data-parent=<?="#accordion".$program['id_program']?> href=<?="#collapseTwo".$program['id_program']?> class="collapsed" aria-expanded="false" >
+	      		 Lista de Cursos <i class=" fa fa-caret-down"></i>
+	      		</a>
+	    	</h4>
+	  	</div>
+	  <div id=<?="collapseTwo".$program['id_program']?> class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+	    <div class="box-body">
+	    	<?php 
+					foreach ($coursesName as $course) { 
+						echo "<p>{$course}</p>";					
+					} ?>
+						
+					</div>				
+    			</div>
+  		</div>								
+<?php
+		} 
+	$teachers = $coursesProgram['teachers'];
+
+	if(!empty($teachers)){ ?>
+		
+		<div class="panel box box-primary">
+          <div class="box-header with-border">
+            <h4 class="box-title">
+              <a data-toggle="collapse" data-parent=<?="#accordion".$program['id_program']?> href=<?="#collapseThree".$program['id_program']?> class="collapsed" aria-expanded="false">
+                Corpo Docente <i class=" fa fa-caret-down"></i>
+              </a>
+            </h4>
+          </div>
+          <div id=<?="collapseThree".$program['id_program']?> class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+            <div class="box-body">
+					<?php 
+						foreach ($teachers as $teacher) { ?>
+
+						<ul id="nav-tabs-wrapper" class="nav nav-tabs nav-pills nav-stacked well">
+							<li class="active">
+								<a href="#vtab1" data-toggle="collapse" data-target=<?="#teacher".$teacher['teacher']?>>
+								<h4><?= $teacher['name']?></h4> 
+								</a>
+							</li>
+						
+								<div id=<?="teacher".$teacher['teacher']?> class="collapse">
+									
+									<b>Resumo:</b><br><?= $teacher['summary']?>
+									<br><br>
+									<a class="btn btn-primary btn-flat btn-xs" href=<?= $teacher['lattes']?>>Currículo Lattes</a>
+								</div>
+			           </ul>
+				<?php	}
+					?>							
+				</div>
+            </div>
+          </div>
+<?php
+	} ?>
+                
+
