8e58045d32b0bf2e12498a650473e469adfd09ee
Italo Paiva <italosensei@hotmail.com>
2016-08-30 15:20:36 -0300

    Fixing migration 131

--- application/migrations/131_cria_permissao_disciplinas_ofertadas.php
+++ application/migrations/131_cria_permissao_disciplinas_ofertadas.php
@@ -5,16 +5,13 @@ class Migration_cria_permissao_disciplinas_ofertadas extends CI_Migration {
 	public function up() {
 
 		#creating permission
-		$this->db->insert('permission', array('permission_name' => "Relatório Geral de Matrícula", 'route' => "enrollment_report", "id_permission"=>35));
+		$this->db->insert('permission', array('permission_name' => "Relatório Geral de Matrícula", 'route' => "enrollment_report", "id_permission"=>36));
 
 		#creating relation between academic secretary and offered disciplines permission
-		$this->db->insert('group_permission', array('id_group' => 11, 'id_permission' => 35));
-
+		$this->db->insert('group_permission', array('id_group' => 11, 'id_permission' => 36));
 	}
 
 	public function down() {
-
-
 
 	}
 
