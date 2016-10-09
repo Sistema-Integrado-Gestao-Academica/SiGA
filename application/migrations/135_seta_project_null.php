387c712bacfa6ab88215b108d5b9b10b08246f57
Emiliemorais <emilie.mo@hotmail.com>
2016-10-09 20:07:43 -0300

    Issue #273 - Setting null on project field on intellectual production

---
+++ application/migrations/135_seta_project_null.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_seta_project_null extends CI_Migration {
+
+	public function up() {
+
+		$this->dbforge->modify_column('intellectual_production', array(
+			'project' => array('type'=> "INT", 'null' => TRUE)
+		));
+
+	    $uk = ("ALTER TABLE intellectual_production ADD CONSTRAINT ID_PROJECT_PRODUCTION_FK FOREIGN KEY (project) REFERENCES academic_project(id)");
+		$this->db->query($uk);
+	}
+
+	public function down(){
+		
+	}
+}
