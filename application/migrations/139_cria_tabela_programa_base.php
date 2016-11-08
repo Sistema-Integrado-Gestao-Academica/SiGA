5c6260cf7cbe4fb7b5604fd37c6607499a7d71d7
Italo Paiva <italosensei@hotmail.com>
2016-11-08 10:59:28 -0200

    Issue #279 - Adding data to migration to populate base programs table

--- application/migrations/139_cria_tabela_programa_base.php
+++ application/migrations/139_cria_tabela_programa_base.php
@@ -16,7 +16,28 @@ class Migration_Cria_tabela_programa_base extends CI_Migration {
 
         $this->dbforge->add_key('id', true);
         $this->dbforge->create_table('base_program_evaluation', true);
+
+        // Test data
+        $this->db->insert_batch('base_program_evaluation', array(
+            array(
+                'name' => "Programa Linguistica UFSC",
+                'start_year' => "2010",
+                'end_year' => "2012",
+                'productions' => "698",
+                'teachers' => "31",
+                'grade' => "6",
+            ),
+            array(
+                'name' => "Programa Linguistica UNICAMP",
+                'start_year' => "2010",
+                'end_year' => "2012",
+                'productions' => "1296",
+                'teachers' => "30",
+                'grade' => "7",
+            ),
+        ));
 	}
+
 	public function down(){
 	}
 }
