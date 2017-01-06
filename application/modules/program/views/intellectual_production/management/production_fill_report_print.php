fb3630d699fe1ee2a269342d1ee490a27dcd0a95
Italo Paiva <italosensei@hotmail.com>
2017-01-06 10:59:49 -0200

    Generating html report to print

--- application/modules/program/views/intellectual_production/management/production_fill_report_print.php
+++ application/modules/program/views/intellectual_production/management/production_fill_report_print.php
@@ -5,13 +5,13 @@
 <h2 class="principal">Relatório de preenchimento de produções do ano de <?= $year ?></h2>
 
 <h4><i class='fa fa-list'></i> Discentes que <?= $msg ?> produziram no ano de <?= $year ?>:</h4>
-<?php listUserProductions($users->students, $filled, $year); ?>
+<?php listUsers($users->students, $year); ?>
 
 <h4><i class='fa fa-list'></i> Docentes que <?= $msg ?> produziram no ano de <?= $year ?>:</h4>
-<?php listUserProductions($users->teachers, $filled, $year); ?>
+<?php listUsers($users->teachers, $year); ?>
 
 <?php
-    function listUserProductions($users, $filled, $year){
+    function listUsers($users, $year){
 
         buildTableDeclaration();
 
@@ -46,9 +46,62 @@
                     echo $user->course_name;
                     echo "</td>";
                 echo "</tr>";
+
+                echo "<tr>";
+                    echo "<td colspan='4'>";
+                        echo "<h4><i class='fa fa-book'></i>Produções de {$user->name}</h4>";
+                        listUserProductions($user, $year);
+                    echo "</td>";
+                echo "</tr>";
             }
         }else{
             callout("info", "Nenhum dado encontrado.");
+        }
+
+        buildTableEndDeclaration();
+    }
+
+    function listUserProductions($user, $year){
+
+        $ci =& get_instance();
+        $ci->load->model("program/production_model");
+        $productions = $ci->production_model->getUserProductions($user->id, $year);
+
+        buildTableDeclaration();
+        buildTableHeaders([
+            'Título',
+            'Ano',
+            'Periódico',
+            'Qualis',
+            'Identificador',
+        ]);
+
+        if(!empty($productions)){
+            foreach ($productions as $production) {
+                echo "<tr>";
+                    echo "<td>";
+                    echo $production->getTitle();
+                    echo "</td>";
+
+                    echo "<td>";
+                    echo $production->getYear();
+                    echo "</td>";
+
+                    echo "<td>";
+                    echo $production->getPeriodic();
+                    echo "</td>";
+
+                    echo "<td>";
+                    echo $production->getQualis();
+                    echo "</td>";
+
+                    echo "<td>";
+                    echo $production->getIdentifier();
+                    echo "</td>";
+                echo "</tr>";
+            }
+        }else{
+            callout("info", "Nenhuma produção encontrada para este usuário.");
         }
 
         buildTableEndDeclaration();
