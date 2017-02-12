da0a079dae044221cd0de3ec3827cd66596d46ad
Italo Paiva <italosensei@hotmail.com>
2017-02-12 14:46:02 -0200

    Configuring travis

--- application/controllers/Utils.php
+++ application/controllers/Utils.php
@@ -1,28 +1,33 @@
 <?php
 
 class Utils extends MX_Controller {
-	
-	public function migrate() {
+
+	public function migrate($type=false) {
 		$this->load->library('migration');
-		if ($this->migration->current()) {
+
+		$migrated = ($type == "latest")
+			? $this->migration->latest()
+			: $this->migration->current();
+
+		if($migrated) {
 			$this->load->test_template("migration_status");
-		} else {
+		}else {
 			show_error($this->migration->error_string());
 		}
 	}
-	
+
 	public function loadSecretaria(){
 		$this->load->template('secretary/index_secretary');
 	}
-	
+
 	public function loadAvaliationAreas(){
 		$this->load->model("program/program_model");
-		
+
 		$lines = file(base_url("area_avaliacao.txt"));
 		foreach ($lines as $lineID => $areaName){
 			$this->program_model->parseProgramAreas($areaName);
 		}
-		
+
 	}
-	
+
 }
--- test.sh
+++ test.sh
@@ -8,12 +8,8 @@ mysql -u $DB_USER -e "CREATE DATABASE siga_test;"
 
 CI_ENV=testing
 
-php index.php utils migrate #Run migration file to create your tables and default user
-
-# php index.php Seeder seed #Add dummy data to your database for purpose of testing
+php index.php utils migrate latest
 
 cd application/tests/
 
 phpunit --coverage-text
-
-eval "cd ../..; exit $?"
