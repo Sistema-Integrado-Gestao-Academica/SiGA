a36cbfa5e70f84ef06b72682166ef61f1e4cec64
Italo Paiva <italosensei@hotmail.com>
2017-04-22 17:15:04 -0300

    Creating service loader to project

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -4,7 +4,7 @@
 require APPPATH."third_party/MX/Loader.php";
 
 class MY_Loader extends MX_Loader {
-    
+
     /**
 	 * Loads the header + content of page + footer at once to display
 	 * @param $pageContent - The page content to load between header and footer
@@ -28,4 +28,44 @@ class MY_Loader extends MX_Loader {
 		$this->view($pageContent, $data);
 		$this->view("test/test_footer.php");
 	}
+
+	/**
+	 * Loads a service.
+	 *
+	 * Services must be placed in a folder called 'services' under
+	 * the application folder or in a module folder.
+	 *
+	 * @param $service - The service class to load
+	 * @param $name - An alias name to access the service
+	 * @return void
+	 */
+	public function service($service, $name='') {
+		$lastSlash = strrpos($service, '/');
+
+		// Is in Modules folder
+		if($lastSlash){
+			$path = substr($service, 0, ++$lastSlash);
+			$serviceName = substr($service, $lastSlash);
+			$path = MODULESPATH.$path;
+		}
+		// Is in Application root folder
+		else{
+			$path = APPPATH;
+			$serviceName = $service;
+		}
+
+		if(empty($name)){
+			$name = $serviceName;
+		}
+
+		$fileName = $path.'services/'.$serviceName.'.php';
+		if(file_exists($fileName)){
+			require_once($fileName);
+			$CI =& get_instance();
+			$CI->$name = new $serviceName();
+		}else{
+			throw new RunTimeException("Unable to locate the service you requested: ".$serviceName);
+		}
+	}
+
 }
