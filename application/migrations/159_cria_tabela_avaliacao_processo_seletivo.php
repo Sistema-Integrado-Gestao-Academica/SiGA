2a917af8cc3f74916be20a7cd2808542ea2bd525
Italo Paiva <italosensei@hotmail.com>
2017-04-26 23:59:21 -0300

    Issue #291 - Fixing migration that creates selection process evaluation table

--- application/migrations/159_cria_tabela_avaliacao_processo_seletivo.php
+++ application/migrations/159_cria_tabela_avaliacao_processo_seletivo.php
@@ -4,19 +4,21 @@ class Migration_cria_tabela_avaliacao_processo_seletivo extends CI_Migration {
 
     public function up() {
 
+        // Add id pk on process_phase table
         $addAsKey = "ALTER TABLE process_phase ADD id int NOT NULL AUTO_INCREMENT primary key FIRST";
         $this->db->query($addAsKey);
 
         // Creating evaluation candidate teacher table
         $this->dbforge->add_field(array(
+            'id' => ['type' => 'INT', 'auto_increment' => TRUE],
             'id_subscription' => ['type' => 'INT'],
             'id_teacher' => ['type' => 'INT'],
-            'grade' => ['type' => 'INT', 'null' => TRUE],
             'id_process_phase' => ['type' => 'INT'],
-            'approved' => ['type' => 'tinyint(1)', 'default' => TRUE],
+            'grade' => ['type' => 'INT', 'null' => TRUE],
+            'approved' => ['type' => 'tinyint(1)', 'default' => FALSE],
         ));
-        
-        $this->dbforge->add_key(array('id_subscription', 'id_teacher', 'process_phase_id'));
+
+        $this->dbforge->add_key('id', TRUE);
         $this->dbforge->create_table('selection_process_evaluation', TRUE);
 
         $subscriptionIdFk = "ALTER TABLE selection_process_evaluation ADD CONSTRAINT IDSUBSCRIPTION_EVALUATION_FK FOREIGN KEY (id_subscription) REFERENCES selection_process_user_subscription(id)";
@@ -27,6 +29,9 @@ class Migration_cria_tabela_avaliacao_processo_seletivo extends CI_Migration {
 
         $processPhaseIdFk = "ALTER TABLE selection_process_evaluation ADD CONSTRAINT EVALUATION_PHASEID_FK FOREIGN KEY (id_process_phase) REFERENCES process_phase(id)";
         $this->db->query($processPhaseIdFk);
+
+        $uk = "ALTER TABLE selection_process_evaluation ADD CONSTRAINT SUBSC_TEACHER_PHASE_UK UNIQUE(id_subscription, id_teacher, id_process_phase)";
+        $this->db->query($uk);
 
     }
 
