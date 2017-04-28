5764e12bc75122310cc2e08eed09636acdf6ac21
Italo Paiva <italosensei@hotmail.com>
2017-04-28 12:18:50 -0300

    Fixing migrations error

--- application/migrations/136_seta_project_null.php
+++ application/migrations/136_seta_project_null.php
@@ -4,15 +4,17 @@ class Migration_seta_project_null extends CI_Migration {
 
 	public function up() {
 
-		$this->dbforge->modify_column('intellectual_production', array(
-			'project' => array('type'=> "INT", 'null' => TRUE)
-		));
+		// Already done in migration 125
 
-	    $uk = ("ALTER TABLE intellectual_production ADD CONSTRAINT ID_PROJECT_PRODUCTION_FK FOREIGN KEY (project) REFERENCES academic_project(id)");
-		$this->db->query($uk);
+		// $this->dbforge->modify_column('intellectual_production', array(
+		// 	'project' => array('type'=> "INT", 'null' => TRUE)
+		// ));
+
+	 //    $uk = ("ALTER TABLE intellectual_production ADD CONSTRAINT ID_PROJECT_PRODUCTION_FK FOREIGN KEY (project) REFERENCES academic_project(id)");
+		// $this->db->query($uk);
 	}
 
 	public function down(){
-		
+
 	}
 }
