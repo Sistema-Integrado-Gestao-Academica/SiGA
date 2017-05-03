261febb8ec7ca83077ef6433fe2716c3b832cc27
Italo Paiva <italosensei@hotmail.com>
2017-05-03 13:26:00 -0300

    Changing migration result when running through CLI

--- application/controllers/Utils.php
+++ application/controllers/Utils.php
@@ -1,28 +1,30 @@
 <?php
 
 class Utils extends MX_Controller {
-	
+
 	public function migrate() {
 		$this->load->library('migration');
 		if ($this->migration->current()) {
-			$this->load->test_template("migration_status");
+			if(!is_cli()){
+				$this->load->test_template("migration_status");
+			}else{
+				echo "Dados migrados com sucesso!";
+			}
 		} else {
 			show_error($this->migration->error_string());
 		}
 	}
-	
+
 	public function loadSecretaria(){
 		$this->load->template('secretary/index_secretary');
 	}
-	
+
 	public function loadAvaliationAreas(){
 		$this->load->model("program/program_model");
-		
+
 		$lines = file(base_url("area_avaliacao.txt"));
 		foreach ($lines as $lineID => $areaName){
 			$this->program_model->parseProgramAreas($areaName);
 		}
-		
 	}
-	
 }
